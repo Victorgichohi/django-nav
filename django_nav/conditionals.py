525f52a4a6e57fc04280d6fda628c010dfcfd10d
pramod gotur <pramodgotur1995@gmail.com>
2020-12-31 15:23:25 +0530

    fix is_authenticated() to is_authenticated

--- django_nav/conditionals.py
+++ django_nav/conditionals.py
@@ -1,5 +1,5 @@
 def user_is_authenticated(context, *args, **kwargs):
-    return context['user'].is_authenticated()
+    return context['user'].is_authenticated
 
 def user_is_staff(context, *args, **kwargs):
     return context['user'].is_staff
